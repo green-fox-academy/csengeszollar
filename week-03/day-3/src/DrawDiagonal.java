9f643ce008339ee1cfa9e0e486b0612d68c4c151
Csenge Szollar <csenge.szollar@gmail.com>
2019-11-14 17:50:50 +0100

    diagonal finished

--- week-03/day-3/src/DrawDiagonal.java
+++ week-03/day-3/src/DrawDiagonal.java
@@ -2,9 +2,40 @@ import java.util.Scanner;
 
 public class DrawDiagonal {
     public static void main(String[] args) {
+
+        // Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %%%%%
+//
+// The square should have as many lines as the number was
+
+
         Scanner number = new Scanner(System.in);
         System.out.print("Please enter a number: ");
         int rows = number.nextInt();
+
+        for (int i = 0; i < rows ; i++) {
+            System.out.print("%");
+            for (int j = 1; j < rows-1; j++) {
+                if (i == 0) {
+                    System.out.print("%");
+                } else if (i == rows - 1) {
+                    System.out.print("%");
+                } else if (i == j){
+                    System.out.print("%");
+                }else System.out.print(" ");
+            }
+            for (int j = rows; j <=rows ; j++) {
+                System.out.print("%");
+            }
+            System.out.println();
+        }
 
 
 

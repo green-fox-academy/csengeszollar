92a90fc3c4dd5513cd5f9aae02353ee3bb5eb266
Csenge Szollar <csenge.szollar@gmail.com>
2019-11-19 12:20:21 +0100

    typo corrected

--- week-08/day-1-OOP/src/PetrolStation/Station.java
+++ week-08/day-1-OOP/src/PetrolStation/Station.java
@@ -7,7 +7,7 @@ public void refill(Car car) {
     this.gasAmount -= car.capacity;
     car.gasAmount++;
     System.out.println("car's gas amount: " + car.gasAmount + " station's gas amount " + this.gasAmount );
-    // kell a string a két mennyiség közés, különben csak simán összeadja a két ampuntot
+    // kell a string a két mennyiség közés, különben csak simán összeadja a két amountot
 }
 
 }

95bb25e05ecd49bd37f12ada6cfcd7a608690c78
Csenge Szollar <csenge.szollar@gmail.com>
2019-11-22 11:18:37 +0100

    recursion GCD done

---
+++ week-08/day-2-recursion/src/GreatestCommonDivisor.java
@@ -0,0 +1,28 @@
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        gcd(36, 24);
+
+        System.out.println(greatestDivisor(75, 15));
+
+
+    }
+
+    public static int greatestDivisor(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+
+        return greatestDivisor(b, a % b);
+    }
+
+    public static void gcd(int a, int b) {
+        int gcd = 1;
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+        System.out.println(gcd);
+    }
+
+}

f2dfb7ba5f8b3c295fb99d022a2ffd406a474357
Csenge Szollar <csenge.szollar@gmail.com>
2020-01-16 18:25:15 +0100

    unit testing for extension completed until median

--- week-11/day-3-unitTesting/src/Extension.java
+++ week-11/day-3-unitTesting/src/Extension.java
@@ -21,9 +21,9 @@ public class Extension {
     int median(List<Integer> pool) {
         Collections.sort(pool);
         if (pool.size() % 2 == 0) {
-            return pool.get((pool.size() - 1) / 2 + pool.size() / 2) / 2;
+            return (pool.get(pool.size() / 2 - 1)) + (pool.get(pool.size() / 2)) / 2;
         }
-        return pool.get((pool.size() - 1 ) / 2);
+        return pool.get((pool.size() - 1) / 2);
     }
 
     boolean isVowel(char c) {

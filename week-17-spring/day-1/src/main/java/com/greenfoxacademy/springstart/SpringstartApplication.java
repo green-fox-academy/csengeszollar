2c84464e383a74ea7750ce1852ae179ccb04c15b
Csenge Szollar <csenge.szollar@gmail.com>
2020-02-13 07:03:16 +0100

    there was no change in this file

--- week-17-spring/day-1/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
+++ week-17-spring/day-1/src/main/java/com/greenfoxacademy/springstart/SpringstartApplication.java
@@ -1,10 +1,12 @@
 package com.greenfoxacademy.springstart;
 
+import com.greenfoxacademy.springstart.controllers.Greeting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 

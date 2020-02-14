ff7fa21c74cd16d74822c1c6d05d71dc1bd42216
Csenge Szollar <csenge.szollar@gmail.com>
2020-02-14 11:11:15 +0100

    Bookcontroller completed

---
+++ week-17-spring/day-2-thymeleaf/src/main/java/com/freenfoxacademy/thymeleafstart/ThymeleafstartApplication.java
@@ -0,0 +1,16 @@
+package com.freenfoxacademy.thymeleafstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@Controller
+
+@SpringBootApplication
+public class ThymeleafstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafstartApplication.class, args);
+    }
+
+}
---
+++ week-17-spring/day-2-thymeleaf/src/test/java/com/freenfoxacademy/thymeleafstart/ThymeleafstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freenfoxacademy.thymeleafstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

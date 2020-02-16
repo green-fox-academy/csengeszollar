8b7f306112ba605a5d3aef2c3bd17604cf1ee3fe
Csenge Szollar <csenge.szollar@gmail.com>
2020-02-16 17:54:26 +0100

    feat table with style sheet

--- week-17-spring/day-3-bankOfSimba/src/main/java/com/greenfox/bankofsimba/controllers/AccountController.java
+++ week-17-spring/day-3-bankOfSimba/src/main/java/com/greenfox/bankofsimba/controllers/AccountController.java
@@ -15,10 +15,16 @@ public class AccountController {
 
     public AccountController() {
         bankAccounts.add(new BankAccount("Simba", 2000, "lion"));
+        bankAccounts.add(new BankAccount("Pumbaa", 200, "warthog"));
+        bankAccounts.add(new BankAccount("Scar", 1600, "lion"));
+        bankAccounts.add(new BankAccount("Azizi", 1150, "hyena"));
+        bankAccounts.add(new BankAccount("Nala", 1700, "lion"));
+        bankAccounts.add(new BankAccount("Rafiki", 600, "mandrill"));
+
     }
 
     @GetMapping("/show")
-    public String showAccounts(Model model) {
+    public String showAccount(Model model) {
         model.addAttribute("account", bankAccounts.get(0));
         return "index";
     }
@@ -28,6 +34,12 @@ public class AccountController {
         model.addAttribute("code1", "<em>HTML</em>");
         model.addAttribute("code2", "<b>Enjoy yourself!</b>");
         return "HTMLception";
+    }
+
+    @GetMapping("/show-all")
+    public String showAccounts(Model model) {
+        model.addAttribute("bankAccounts", bankAccounts);
+        return "table";
     }
 
 

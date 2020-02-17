7af9e19c37529b938b3f19b59ab860562b32076b
Csenge Szollar <csenge.szollar@gmail.com>
2020-02-17 20:15:29 +0100

    Bank of Simba feat add new account

--- week-17-spring/day-3-bankOfSimba/src/main/java/com/greenfox/bankofsimba/controllers/AccountController.java
+++ week-17-spring/day-3-bankOfSimba/src/main/java/com/greenfox/bankofsimba/controllers/AccountController.java
@@ -4,6 +4,9 @@ import com.greenfox.bankofsimba.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,13 +17,12 @@ public class AccountController {
     private List<BankAccount> bankAccounts = new ArrayList<>();
 
     public AccountController() {
-        bankAccounts.add(new BankAccount("Simba", 2000, "lion"));
-        bankAccounts.add(new BankAccount("Pumbaa", 200, "warthog"));
-        bankAccounts.add(new BankAccount("Scar", 1600, "lion"));
-        bankAccounts.add(new BankAccount("Azizi", 1150, "hyena"));
-        bankAccounts.add(new BankAccount("Nala", 1700, "lion"));
-        bankAccounts.add(new BankAccount("Rafiki", 600, "mandrill"));
-
+        bankAccounts.add(new BankAccount("Simba", 2000, "lion", true, true));
+        bankAccounts.add(new BankAccount("Pumbaa", 200, "warthog", false, true));
+        bankAccounts.add(new BankAccount("Scar", 1600, "lion", true, false));
+        bankAccounts.add(new BankAccount("Azizi", 1150, "hyena", false, false));
+        bankAccounts.add(new BankAccount("Nala", 1700, "lion", false, true));
+        bankAccounts.add(new BankAccount("Rafiki", 600, "mandrill", false, true));
     }
 
     @GetMapping("/show")
@@ -40,6 +42,32 @@ public class AccountController {
     public String showAccounts(Model model) {
         model.addAttribute("bankAccounts", bankAccounts);
         return "table";
+    }
+
+
+    @PostMapping("account-raise")
+    public String raiseBalance(String nameSelected){
+
+        for (int i = 0; i < bankAccounts.size(); i++) {
+            if (bankAccounts.get(i).getName().equals(nameSelected)) {
+                if (bankAccounts.get(i).isKing()) {
+                    bankAccounts.get(i).setBalance(bankAccounts.get(i).getBalance() + 100);
+                } else {
+                    bankAccounts.get(i).setBalance(bankAccounts.get(i).getBalance() + 10);
+                }
+            }
+        } return "redirect:/show-all";
+    }
+
+    @GetMapping("/add-account")
+    public String addAccount(){
+        return "addAccount";
+    }
+
+    @PostMapping("/add-account")
+    public String submitAccount(@ModelAttribute BankAccount account){
+        bankAccounts.add(account);
+        return "redirect:/show-all";
     }
 
 
--- week-17-spring/day-3-bankOfSimba/src/main/java/com/greenfox/bankofsimba/models/BankAccount.java
+++ week-17-spring/day-3-bankOfSimba/src/main/java/com/greenfox/bankofsimba/models/BankAccount.java
@@ -5,11 +5,15 @@ public class BankAccount {
     private String name;
     private int balance;
     private String animalType;
+    private boolean isKing;
+    private boolean isGood;
 
-    public BankAccount(String name, int balance, String animalType) {
+    public BankAccount(String name, int balance, String animalType, boolean isKing, boolean isGood) {
         this.name = name;
         this.balance = balance;
         this.animalType = animalType;
+        this.isKing = isKing;
+        this.isGood = isGood;
     }
 
     public String getName() {
@@ -34,5 +38,21 @@ public class BankAccount {
 
     public void setAnimalType(String animalType) {
         this.animalType = animalType;
+    }
+
+    public boolean isKing() {
+        return isKing;
+    }
+
+    public void setKing(boolean king) {
+        isKing = king;
+    }
+
+    public boolean isGood() {
+        return isGood;
+    }
+
+    public void setGood(boolean good) {
+        isGood = good;
     }
 }

df776f7c63033084d7d8d60a64fed349eb1e7b24
Csenge Szollar <csenge.szollar@gmail.com>
2020-02-20 14:51:41 +0100

    utility service feat colored background completed

---
+++ week-18-DIC/day-1/src/main/java/com/grenfoxacademy/dependencies/UtilityService/controller/UtilityController.java
@@ -0,0 +1,38 @@
+package com.grenfoxacademy.dependencies.UtilityService.controller;
+
+import com.grenfoxacademy.dependencies.UtilityService.services.UtilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UtilityController {
+
+    private UtilityService utilityService;
+
+    @Autowired
+    public UtilityController (UtilityService utilityService) {
+        this.utilityService = utilityService;
+    }
+
+    @GetMapping("/useful")
+    public String utilities() {
+        return "useful";
+    }
+
+    @GetMapping("/useful/colored")
+    public String backgroundColor(Model model){
+        model.addAttribute("backgroundColor", utilityService.randomColor());
+        return "background-color";
+    }
+
+    @GetMapping("/useful/email")
+    public String validEmail(@RequestParam(name = "email", required = false) String email, Model model) {
+        model.addAttribute("email", utilityService.validateEmail(email));
+        return "email-validation";
+    }
+
+
+}

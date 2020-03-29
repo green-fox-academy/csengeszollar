ba538d4efeccdca4801714be8524dfef628e5d36
Csenge Szollar <csenge.szollar@gmail.com>
2020-03-29 20:56:50 +0200

    appendA corrected writing out only the appended field

--- week-23-REST/src/main/java/com/greenfoxacademy/rest/models/AppendA.java
+++ week-23-REST/src/main/java/com/greenfoxacademy/rest/models/AppendA.java
@@ -1,21 +1,13 @@
 package com.greenfoxacademy.rest.models;
 
 public class AppendA {
-    private String appendable;
+
     private String appended;
 
     public AppendA(String appendable) {
-        this.appendable = appendable;
         this.appended = appendable + "a";
     }
 
-    public String getAppendable() {
-        return appendable;
-    }
-
-    public void setAppendable(String appendable) {
-        this.appendable = appendable;
-    }
 
     public String getAppended() {
         return appended;

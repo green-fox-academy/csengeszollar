9609891749666e81f533bd5d25f4f566a4443558
Csenge Szollar <csenge.szollar@gmail.com>
2020-05-08 01:13:35 +0200

    small syntax modification

--- week-26-JS-React/JavaScript/data-structures/list-introduction.js
+++ week-26-JS-React/JavaScript/data-structures/list-introduction.js
@@ -10,10 +10,56 @@ console.log(names[2]);
 names.forEach(name => {
     console.log(name);
 });
-names.splice(1,1);
-names.reverse().forEach(name => {
-    console.log(name);
-});
+names.splice(1, 1);
+// names.reverse().forEach(name => {
+//     console.log(name);
+// });
+
+for (let i = names.length - 1; i >= 0; i--) {
+    console.log(names[i]);
+}
 
 names.splice(0);
 console.log(names.length);
+
+// class Animals{
+//
+//
+//     constructor(age, type, legs) {
+//         this._age = age;
+//         this._type = type;
+//         this._legs = legs;
+//     }
+//
+//
+//     get age() {
+//         return this._age;
+//     }
+//
+//     set age(value) {
+//         this._age = value;
+//     }
+//
+//     get type() {
+//         return this._type;
+//     }
+//
+//     set type(value) {
+//         this._type = value;
+//     }
+//
+//     get legs() {
+//         return this._legs;
+//     }
+//
+//     set legs(value) {
+//         this._legs = value;
+//     }
+// }
+//
+// let zebra = new Animals(4, "zebra", 4);
+// console.log(zebra);
+
+
+
+

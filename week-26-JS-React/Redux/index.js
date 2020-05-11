dd43663ac37e0706064438bcdf42b9fedabb91bb
Csenge Szollar <csenge.szollar@gmail.com>
2020-05-11 12:47:53 +0200

    with multiple reducers as two properties of an initial object

--- week-26-JS-React/Redux/index.js
+++ week-26-JS-React/Redux/index.js
@@ -2,6 +2,7 @@ const redux = require('redux')
 const createStore = redux.createStore
 // action creator
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
     return {
@@ -10,15 +11,28 @@ function buyCake() {
    }
 }
 
+function buyIceCream() {
+    return {
+        type: BUY_ICECREAM
+    }
+}
+
 // reducer
 const initialState = {
-    numOfCake: 10
+    numOfCake: 10,
+    numOfIceCream: 20
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
         case BUY_CAKE: return {
+            ...state,
             numOfCake: state.numOfCake - 1
+        }
+        case BUY_ICECREAM: return {
+            // with multiple reducers is important to spread state
+            ...state,
+            numOfIceCream: state.numOfIceCream - 1
         }
         default: return state
     }
@@ -28,4 +42,6 @@ const store = createStore(reducer)
 console.log('initial state', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Updated state', store.getState()))
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
